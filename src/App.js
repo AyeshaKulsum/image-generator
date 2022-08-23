02bdd6e44f082a825b67e5d17aa34eb1a18ddc0f
Ayesha Kulsum S J <ayeshakulsum8@gmail.com>
2022-08-23 18:00:43 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import './App.css';
 import * as htmlToImage from 'html-to-image';
 import * as FileSaver from 'file-saver';
 function App() {
-  const numbers = [0, 25, 50, 75, 100]
+  const numbers = [
+    // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    // 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
+    //  20, 21, 22, 23, 24, 25, 26, 27, 28, 29
+    //  30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+    //  40, 41, 42, 43, 44, 45, 46, 47, 48, 49
+    //  50, 51, 52, 53, 54, 55, 56, 57, 58, 59
+    // 60, 61, 62, 63, 64, 65, 66, 67, 68, 69
+    //  70, 71, 72, 73, 74, 75, 76, 77, 78, 79
+    //  80, 81, 82, 83, 84, 85, 86, 87, 88, 89
+    //  90, 91, 92, 93, 94, 95, 96, 97, 98, 99
+     100
+  ]
 
   const downloadElement = async () => {
     console.log('hello');
@@ -29,8 +41,8 @@ function App() {
       <br />
       <br />
       {numbers && numbers.map(num => {
-        return <> <div id={`diagram_${num}`} style={{ "background-color": '#fffff' }}>
-          <div className="progress" >
+        return <> <div id={`diagram_${num}`} style={{ "background-color": '#E7E9ED;', width: "240px", height: "20px" }}>
+          <div className="progress" style={{ "border-radius": "0" }}  >
             <div className="progress-bar" role="progressbar" style={{ width: `${num}%`, "background-color": '#57599F' }} aria-valuenow={num} aria-valuemin="0" aria-valuemax="100"></div>
           </div>
         </div>
